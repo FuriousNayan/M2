94398a7ae1fdc920a88e23f5df2e89ae07e88799
FuriousNayan <npatel24@my.waketech.edu>
2025-01-13 13:54:23 -0500

    M2 Notes for 1/13/25

---
+++ index.js
@@ -0,0 +1,35 @@
+// Javascript Code goes here
+console.log('Internal JS console message. Hello, Mr. Gardner')
+console.log(document)
+// console.dir(document)
+
+// ALERTS //
+// alert("Hello World")  // Textbox pop-up
+
+// window.onload = function(){
+//     alert(`Hello Mr. Gardner`)
+// }
+
+
+// PROMPTS //
+// const student = prompt('What is your name?')
+// console.log(student)
+// writes to document, but doesn't work with script defer 
+// document.write(`<p>Hello, ${student}`);
+
+// modern web dev standard 
+const student = prompt('What is your name?');
+const messageElement = document.createElement("p"); // Create a new <p> element
+messageElement.textContent = `Hello, ${student}!` // Sets the text content of that
+document.body.appendChild(messageElement) // Append the new <p> to <body>
+// This ensures the content is dynamically added without overwriting any page content. 
+
+
+// WRITE TO ELEMENT BY IDs //
+document.getElementById("myH1").innerHTML = 'I love JavaScript';
+document.getElementById('myParagraph').textContent = 'Hello, this is a paragraph'
+
+// Styling page elements //
+document.body.style.color = 'green'
+document.body.style.backgroundColor = 'black'
+document.body.style.fontSize = '2em'
