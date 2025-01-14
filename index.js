2d14ff17717a85d5edc57bfbc7c9f4e04506160b
FuriousNayan <npatel24@my.waketech.edu>
2025-01-14 13:12:56 -0500

    M2 Notes 1/14/25

--- index.js
+++ index.js
@@ -33,3 +33,32 @@ document.getElementById('myParagraph').textContent = 'Hello, this is a paragraph
 document.body.style.color = 'green'
 document.body.style.backgroundColor = 'black'
 document.body.style.fontSize = '2em'
+
+// EVENT LISTENER //
+document.getElementById('myH1').addEventListener('click', function(){
+    // function details go here.
+    document.getElementById("myH1").style.color = 'red'
+})
+
+document.getElementById('myH1').addEventListener('mouseover', function(){
+    document.getElementById('myH1').style.color = 'orange'
+})
+
+document.getElementById('myParagraph').addEventListener('dblclick', function(){
+    document.getElementById('myParagraph').style.color = 'white'
+})
+
+document.getElementById('myButton').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'white'
+
+})
+
+document.getElementById('myButton2').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'black'
+
+})
+
+// Difference Between .innerHTML and .textContent //
+console.log(document.getElementById('myParagraph2').innerHTML)  //Output: '<b>text</b>
+
+console.log(document.getElementById("myParagraph3").textContent) //Output: "text"
